c84c81ed27702eef448be412113943dc035a2b59
Aman Sharma <aman703jk@gmail.com>
2022-10-05 14:22:31 +0530

    map products and remove cart functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,37 @@
 import { addToCart, emptyCart, removeToCart } from '../actions/cartActions'
 import { useDispatch, useSelector } from 'react-redux'
 import { productList } from '../actions/productActions';
+import { useEffect } from 'react';
 
 
 function Main() {
   const dispatch = useDispatch();
   let data = useSelector((state) => state.productData);
-  const product = {
-    name: 'i Phone',
-    category: 'mobile',
-    price: 10000,
-    color: 'red'
-  }
+  console.warn("data in main component", data);
+
+  useEffect(() => {
+    dispatch(productList())
+  }, [])
   return (
     <div>
-      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
-      <div>
-        <button onClick={() => dispatch(removeToCart(product.name))}>Remove from Cart</button>
-      </div>
       <div>
         <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
       </div>
-      <div>
-        <button onClick={() => dispatch(productList())}>Get Product List</button>
+      <div className='product-container'>
+        {
+          data.map((item) => <div className='product-item'>
+            <img src={item.photo} alt="" />
+            <div>Name : {item.name} </div>
+            <div>Color : {item.color} </div>
+            <div>Price : {item.price} </div>
+            <div>Category : {item.category} </div>
+            <div>Brand : {item.brand} </div>
+            <div >
+              <button onClick={() => dispatch(addToCart(item))} >Add to Cart</button>
+              <button onClick={() => dispatch(removeToCart(item.id))}>Remove to Cart</button>
+            </div>
+          </div>)
+        }
       </div>
     </div>
   );
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -9,8 +9,8 @@ export const cartData = (data = [], action) => {
             return [action.data, ...data]
         case REMOVE_FROM_CART:
             console.warn("REMOVE_FROM_CART condition ", action);
-            data.length = data.length ? data.length - 1 : []
-            return [...data]
+            const restItems = data.filter((item) => item.id !== action.data)
+            return [...restItems]
         case EMPTY_CART:
             console.warn("EMPTY CART condition ", action);
             data = []
