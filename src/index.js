9bfdfb64007b3b2676926814033c865e0c44b870
Aman Sharma <aman703jk@gmail.com>
2022-10-05 14:56:40 +0530

    display selected product on cart page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store} >
-    <App />
+    <BrowserRouter> <App /></BrowserRouter>
   </Provider>
 );
 
